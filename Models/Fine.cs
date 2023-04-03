0c1a06d9c27fab8ec605e29164bb6fa693d6b704
Jordan McComb <jordanmccomb712@gmail.com>
2023-04-03 16:12:12 +0100

    changed fine and testing models

--- Models/Fine.cs
+++ Models/Fine.cs
@@ -1,9 +1,18 @@
 public class Fine{
+
+    public string FineID {get; set; }
+    public string FullName {get; set;}
     public double AmountDue {get; set;} //in GBP
     public DateTime DateIssued {get; set;}
 
-    public Fine (double amountDue, DateTime dateIssued){
+    public bool IsPaid {get; set;}
+
+    public Fine(string fineID, string fullName, double amountDue, DateTime dateIssued, bool isPaid)
+    {
+        this.FineID = fineID;
+        this.FullName = fullName;
         this.AmountDue = amountDue;
         this.DateIssued = dateIssued;
+        this.IsPaid = isPaid;
     }
 }
